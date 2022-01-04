aa6973288d2f1813b22d6f0933c5d845fbd07205
alliano <allianoanonymous@gmail.com>
2022-01-04 01:19:12 -0500

    seter geter

--- test/src/App.java
+++ test/src/App.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
@@ -10,8 +11,9 @@ public class  App {
     private static final String sea = "0123456789";
     private static final String shep = "@";
     private static final String shepC = "#";
-    public static final String userDead = "!";
-    public static final String compDead = "-";
+    public static final String userDead = "X";
+    public static final String compDead = "!";
+    public static final String missShoot = "-";
     public static final Scanner terminalInput = new Scanner(System.in);
     public static final Random valueRandom = new Random();
     public static List<List<String>> arena = new ArrayList<>();
@@ -23,13 +25,12 @@ public class  App {
         System.out.println("right now, the sea is empety");
         loopSea();
         newArena(arenaDummy);
-
         loop();       
     }
     
     private static void loop(){
+            System.out.println("Deploy Player Ships ");
             arena = loopSea();
-            
             int[][] userChoose = Shep();
             int[][] computer = Computer(userChoose);
             System.out.println(Arrays.deepToString(computer));
@@ -67,20 +68,21 @@ public class  App {
         int[][] cordinat = new int[5][2];
         int cordinat_y,cordinat_x;
         String[] nameShep = {
-            "Shep1",
-            "Shep2",
-            "Shep3",
-            "Shep4",
-            "Shep5",
+            "Ship1",
+            "Ship2",
+            "Ship3",
+            "Ship4",
+            "Ship5",
         };
         for (int i = 0; i < 5; i++) {
+            try{
                 System.out.println(nameShep[i]);
                 System.out.print("Enter cordinate Y : ");
                 cordinat_x = terminalInput.nextInt();
                 System.out.print("Enter cordinat X : ");
                 cordinat_y = terminalInput.nextInt(); 
                 System.out.println("\n"); 
-
+    
                 String cordinat_x_y = arena.get(cordinat_y).get(cordinat_x);
                 arena.get(cordinat_y).set(cordinat_x, shep);
                 
@@ -91,6 +93,12 @@ public class  App {
                     cordinat[i][0] = cordinat_y;
                     cordinat[i][1] = cordinat_x;
                 }
+                
+            }catch(InputMismatchException handling){
+                System.out.println("You Must Write A Number");
+                continue;
+            }
+                
         }
         return cordinat;
     }
@@ -145,28 +153,37 @@ private static void batle(int[][] computer,List<List<String>> arena){
     
     for (int i = 0; i < computer.length; i++) {
         System.out.println("YOU TURN");
-        
-        System.out.print("input cordinat y _comp : ");
-        int userChoose_y = terminalInput.nextInt();
-        System.out.print("input cordinat x _comp : ");
-        int userChoose_x = terminalInput.nextInt();
-
-            if(arena.get(userChoose_y).get(userChoose_x).equals(shepC)){
-                arenaDummy.get(userChoose_y).set(userChoose_x, compDead);
-                arena.get(userChoose_y).set(userChoose_x, compDead);
-                clearSc();
-                newArena(arenaDummy);
-                System.out.println("YOU ARE WIN");
-                user = user + 1;
-            }else if(arena.get(userChoose_y).get(userChoose_x).equals("-")){
-                System.out.println("COMPUTER HAS BEEN DEAD PLEASE CHOOSE SOME ELSE");
-            }else{
-                clearSc();
-                arenaDummy.get(userChoose_y).set(userChoose_x, "!");
-                arena.get(userChoose_y).set(userChoose_x, "!");
-                newArena(arenaDummy);
-                System.out.println("You missed");
-            }
+        try{
+            System.out.print("input cordinat y _comp : ");
+            int userChoose_y = terminalInput.nextInt();
+            System.out.print("input cordinat x _comp : ");
+            int userChoose_x = terminalInput.nextInt();
+    
+                if(arena.get(userChoose_y).get(userChoose_x).equals(shepC)){
+                    arenaDummy.get(userChoose_y).set(userChoose_x, compDead);
+                    arena.get(userChoose_y).set(userChoose_x, compDead);
+                    clearSc();
+                    newArena(arenaDummy);
+                    System.out.println("Boom! You Sunk The Ship!");
+                    user = user + 1;
+                }else if(arena.get(userChoose_y).get(userChoose_x).equals(compDead)){
+                     System.out.println("You Can't Hit In The Same Place");
+                }else if(arena.get(userChoose_y).get(userChoose_x).equals(shep)){
+                    System.out.println("Oh No, You Sunk Your Own Ship :(");
+                    arena.get(userChoose_y).set(userChoose_x,userDead);
+                    arenaDummy.get(userChoose_y).set(userChoose_x,userDead);
+                }else{
+                    clearSc();
+                    arenaDummy.get(userChoose_y).set(userChoose_x,missShoot );
+                    arena.get(userChoose_y).set(userChoose_x, missShoot);
+                    newArena(arenaDummy);
+                    System.out.println("You missed");
+                }
+        }catch(InputMismatchException handling){
+            System.out.println("You Must Write A Number");
+            terminalInput.next();
+            continue;
+        }
             
        int _cordinat_y_comp = valueRandom.nextInt(9); 
        int _cordinat_x_comp = valueRandom.nextInt(9); 
@@ -175,7 +192,7 @@ private static void batle(int[][] computer,List<List<String>> arena){
            System.out.println("COMPUTER TURN");
            Thread.sleep(2000);
            if (arena.get(_cordinat_y_comp).get(_cordinat_x_comp).equals(shep)){
-               System.out.println("Computer win");
+               System.out.println("The Computer Sunk Your Ship");
                arenaDummy.get(_cordinat_y_comp).set(_cordinat_x_comp, userDead);
                arena.get(_cordinat_y_comp).set(_cordinat_x_comp, userDead);
                newArena(arenaDummy);
@@ -183,8 +200,8 @@ private static void batle(int[][] computer,List<List<String>> arena){
                 comp += 1;
            }else{
                clearSc();
-               arena.get(_cordinat_y_comp).set(_cordinat_x_comp, "!");
-               arenaDummy.get(_cordinat_y_comp).set(_cordinat_x_comp, "!");
+               arena.get(_cordinat_y_comp).set(_cordinat_x_comp, missShoot);
+               arenaDummy.get(_cordinat_y_comp).set(_cordinat_x_comp,missShoot);
                newArena(arenaDummy);
                System.out.println("cordinat_y -> "+_cordinat_y_comp);
                System.out.println("cordinat_x -> "+_cordinat_x_comp);
@@ -196,17 +213,19 @@ private static void batle(int[][] computer,List<List<String>> arena){
 
     }
     if (user > comp){
-        System.out.println("secore User --> " + user);
-        System.out.println("secore comp --> " + comp);
+        System.out.println("\nyour ship : " + user + " computer ships : " + comp);
         System.out.println("Hooray, you winer the battle");
+        System.out.println("====================================");
     }else if (user < comp) {
-        System.out.println("secore User --> " + user);
-        System.out.println("secore comp --> " + comp);
-        System.out.println("YOU ARE LOSE");
+        System.out.println("\nyour ship : " + user + " computer ships : " + comp);
+        System.out.println("Sorry, You Lost The Battle :v");
+        System.out.println("====================================");
+        System.out.println("you lose");
     }else if(user == comp){
-        System.out.println("secore User --> " + user);
-        System.out.println("secore comp --> " + comp);
-        System.out.println("DROW");
+        System.out.println("\nyour ship : " + user + " computer ships : " + comp);
+        System.out.println("the batle is drow");
+        System.out.println("====================================");
+        
     }
 
 }
